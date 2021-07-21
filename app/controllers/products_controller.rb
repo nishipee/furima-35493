3d2a12f93105b6f4c8746c41651b920689e27046
nishipee <sn.haryo.000@gmail.com>
2021-07-21 16:55:24 +0900

    商品情報編集機能

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,7 +1,9 @@
 class ProductsController < ApplicationController
-  before_action :authenticate_user!, only: [:new, :create]
+  before_action :authenticate_user!, only: [:new, :create, :edit, :update]
+  before_action :move_to_index, only: [:edit, :update]
+
   def index
-    @products = Product.all.order("created_at DESC")
+    @products = Product.includes(:user).order("created_at DESC")
   end
 
   def new
@@ -20,9 +22,29 @@ class ProductsController < ApplicationController
   def show
     @product = Product.find(params[:id])
   end
+
+  def edit
+    @product = Product.find(params[:id])
+  end
+
+  def update
+    @product = Product.find(params[:id])
+    if @product.update(product_params)
+      redirect_to root_path
+    else
+      render :edit
+    end
+  end
   
   private
   def product_params
     params.require(:product).permit(:image, :name, :introduction, :category_id, :status_id, :charge_id, :area_id, :scheduled_day_id, :price).merge(user_id: current_user.id)
+  end
+
+  def move_to_index
+    @product = Product.find(params[:id])
+    if @product.user != current_user || @product.purchase_history.present?
+      redirect_to root_path
+    end
   end
 end
