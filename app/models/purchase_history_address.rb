f242c6932f8078c1ac845ae7e40cabc3b24f1214
nishipee <sn.haryo.000@gmail.com>
2021-07-28 01:31:15 +0900

    商品購入機能の実装-修正

--- app/models/purchase_history_address.rb
+++ app/models/purchase_history_address.rb
@@ -4,7 +4,6 @@ class PurchaseHistoryAddress
 
   with_options presence: true do
     validates :token
-    validates :price
     validates :postcode, format: { with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Enter it as follows (e.g. 123-4567)" }
     validates :area_id, numericality: { other_than: 1, message: "can't be blank" }
     validates :city
